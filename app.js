8a5d5713a89aed9b377b537846f5ee224a631aae
Zach Dive <zjhd201@exeter.ac.uk>
2021-09-01 09:28:05 +0100

    bye bye brain

--- app.js
+++ app.js
@@ -13,13 +13,28 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(__dirname + "/views/partials");
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render("index");
 });
+ 
+//Modern way to consume promises  
+app.get('/beers', async (req, res) => {
+   const beers = await punkAPI.getBeers()
+   res.render('beers', { beers });
+});
+
+app.get('/random-beers', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom()
+  res.render('random-beers', { randomBeer });
+});
+
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
